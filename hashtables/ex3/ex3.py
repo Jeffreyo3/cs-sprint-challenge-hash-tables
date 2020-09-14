6b9f17e1ff505b0f3c5ca7b7fc68b8449d49f84f
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-09-13 20:40:29 -0700

    ex3 tests passing

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,43 @@
 def intersection(arrays):
     """
-    YOUR CODE HERE
+    
     """
-    # Your code here
+    # calculate how many arrays there are
+    numOfLists = len(arrays)
 
-    return result
+    # base case
+    if numOfLists == 0:
+        return None
+
+    # if only one array, return array
+    # at zero idx since all nums are in that array
+    if numOfLists == 1:
+        return arrays[0]
+
+    # for 2 or more arrays:
+    # create hashtable/dictionary
+    numberDict = {}
+    
+    # loop through array of arrays
+    for array in arrays:
+        # loop through all numbers in current array
+        for number in array:
+            # increment 'count' in dictionary for each number
+            if number not in numberDict:
+                numberDict[number] = 1
+            else:
+                numberDict[number] += 1
+    # make a list to store common numbers
+    commonNums = []
+    # go through all unique numbers found in the array of arrays
+    for key, value in numberDict.items():
+        # if the number has shown up the same number of times 
+        # as there are arrays in the arrays list, add to list
+        if value == numOfLists:
+            commonNums.append(key)
+
+
+    return commonNums
 
 
 if __name__ == "__main__":
