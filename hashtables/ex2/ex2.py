d9e979e6e3a368b92e7d167dca399a85dfe8a336
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-09-13 20:25:57 -0700

    ex2 passing tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,3 @@
-#  Hint:  You may not need all of these.  Remove the unused functions.
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -9,6 +8,30 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # base case
+    if length <= 1:
+        return ["NONE"]
+        
+    # create hashtable/dictionary to store destinations
+    destinationDict = {}
+    # preallocate array memory
+    route = ['NONE'] * length
+    
+    # add routes to dictionary
+    for ticket in tickets:
+        destinationDict[ticket.source] = ticket.destination
+
+    # print(destinationDict)
+
+    # reconstruct trip by enumerating through dictionary
+
+    # set starting location
+    route[0] = destinationDict["NONE"]
+    # go through each destination and insert the next destination's value
+    # by checking the current index's key.
+    for i in range(length - 1):
+        route[i+1] = destinationDict[route[i]]
+    
+    print(route)
 
     return route
